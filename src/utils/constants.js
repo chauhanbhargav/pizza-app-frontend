d4100a4f1bcb8206bca142cb5ec877066ae1a4de
Bhargav Chauhan <bhargavchauhan777@gmail.com>
2020-05-19 13:50:01 +0530

    Changes in readme.md and change host for api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:8000/api/v1/";
+export const API_BASE_URL = "http://api.bhargavchauhan.in/api/v1/";
 
 export const COOKIE_KEY = "pizzaUUID";
 export const DeliveryFee = 10;
